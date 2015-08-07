b06cf67f13d8949af48c9db22e6445aca81a5d6a
john <chihung.siu@magario.com>
2015-08-07 14:58:07 +0800

    show cate in the new page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,22 +1,42 @@
+var oneSection=[];
 angular.module('catalog.controllers', [])
-
     .controller('HomeCtrl', function ($scope, $http, $ionicModal, $timeout) {
         $http.get('http://escgroup.net/').then(function (res) {
             console.log(' \n\n\n\n\n\nrestarted')
             $scope.categories = res.data;
-            $scope.sections = $scope.categories.sections;
-            $scope.inOneCata= false;
-            $scope.numtwo= 0;
-            $scope.clickTest = function (){
-                alert('receive click');
-            }
-            $scope.showCata= function (index){
-                console.log("$scope \n",$scope.categories);
-                $scope.inSubCata=!$scope.inSubCata;
-                $scope.newInfo="http://escgroup.net/site/assets/files/1019/p_10_1.0x100.jpg?nc=1438425728";
-                $scope.newInfo2=$scope.categories[0].sections[2].title;
-                console.log('test \n',index);
-                //console.log('test \n',$event);
+            $scope.subCateView= -1;
+            $scope.numtwo= 2;
+            $scope.oneSection=[];
+            var i,j=0;
+            $scope.filterIt=function(){
+                var _it=$scope.categories;
+                $scope.oneSection=[];
+                for(i=0;i<_it.length;i++){
+                    if(i==$scope.subCateView){
+                        //console.log(i,"\n",$scope.subCateView);
+                        //console.log(_it,'\n','index: ',i);
+                        var _section=_it[i].sections;
+                        for(j=0;j<_section.length;j++){
+                            //console.log(_section[j].title);
+                            $scope.oneSection.push( _section[j]);
+                        }
+                    }
+                }
+                oneSection=$scope.oneSection;
+                //console.log('onesection: ',$scope.oneSection);
+
+            };
+            $scope.objectKeys=function(_it,_index){
+                if(_index<2){
+                    return false;
+                }
+                return false;
+            };
+            $scope.setCateView= function (index){
+                $scope.inSubCate=!$scope.inSubCate;
+                $scope.subCateView=index;
+                //$scope.newInfo="http://escgroup.net/site/assets/files/1019/p_10_1.0x100.jpg?nc=1438425728";
+                //$scope.newInfo2=$scope.categories[0].sections[2].title;
             }
         }, function (err) {
             console.error("HOME", err);
@@ -27,6 +47,9 @@ angular.module('catalog.controllers', [])
     .controller('CategoryCtrl', function ($scope, $http, $ionicModal, $timeout) {
         $http.get('http://escgroup.net/').then(function (res) {
             $scope.categories = res.data;
+            $scope.numtwo= 2;
+            var i,j=0;
+            $scope.oneSection=oneSection;
         }, function (err) {
             console.error("HOME", err);
         });
