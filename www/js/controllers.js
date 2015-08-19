616d1aea1cf11702e09aaff0e0cc0e07240434f7
Kenta Iwasaki <dranithix@gmail.com>
2015-08-20 01:57:14 +0800

    Fixed minor bug for the cover.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,85 +1,81 @@
 var module = angular.module('catalog.controllers', [])
-    .controller('CoverCtrl', function ($scope, $state, $http, $ionicSlideBoxDelegate, $timeout) {
-        $http.get('http://escgroup.net/').then(function (res) {
-            $scope.categories = res.data;
-        }, function (err) {
-            console.error("HOME", err);
-        });
-        $scope.slideHasChanged = function($index) {
-            if($index==2) {
+        .controller('CoverCtrl', function ($scope, $state, $ionicHistory, $ionicSlideBoxDelegate, $timeout) {
+            $scope.slideHasChanged = function (index) {
+                if (index == 2) {
+                    $state.go('home');
+                    $timeout(function () {
+                        $ionicSlideBoxDelegate.slide(0, 0);
+                    }, 300);
+                }
+            }
+        })
+        .controller('HomeCtrl', function ($scope, $state, $http, $ionicModal, $timeout) {
+            $http.get('http://escgroup.net/').then(function (res) {
+                $scope.categories = res.data;
+            }, function (err) {
+                console.error("HOME", err);
+            });
+
+            $scope.selectCategory = function ($category) {
+                $state.go('category', {category: $category});
+            }
+
+        })
+
+        .controller('CategoryCtrl', function ($scope, $state, $ionicHistory, $stateParams, $http, $ionicModal, $timeout) {
+            if ($stateParams.category == null) {
                 $state.go('home');
                 $ionicHistory.clearHistory();
                 $ionicHistory.clearCache();
             }
-        }
-    })
-    .controller('HomeCtrl', function ($scope, $state, $http, $ionicModal, $timeout) {
-        $http.get('http://escgroup.net/').then(function (res) {
-            $scope.categories = res.data;
-        }, function (err) {
-            console.error("HOME", err);
-        });
 
-        $scope.selectCategory = function ($category) {
-            $state.go('category', {category: $category});
-        }
+            $scope.category = $stateParams.category;
+            $scope.sections = $scope.category.sections;
 
-    })
+            $scope.selectSection = function ($section) {
+                $state.go('section', {section: $section});
+            }
+        })
 
-    .controller('CategoryCtrl', function ($scope, $state, $ionicHistory, $stateParams, $http, $ionicModal, $timeout) {
-        if ($stateParams.category == null) {
-            $state.go('home');
-            $ionicHistory.clearHistory();
-            $ionicHistory.clearCache();
-        }
+        .controller('SectionCtrl', function ($scope, $ionicHistory, $compile, $sce, $state, $stateParams, $http, $ionicModal, $timeout) {
+            if ($stateParams.section == null) {
+                $state.go('home');
+                $ionicHistory.clearHistory();
+                $ionicHistory.clearCache();
+            }
+            $scope.section = $stateParams.section;
 
-        $scope.category = $stateParams.category;
-        $scope.sections = $scope.category.sections;
+            $http.get('http://' + $scope.section.url).then(function (res) {
+                $scope.sectionData = res.data;
+            }, function (err) {
+                console.error("HOME", err);
+            });
 
-        $scope.selectSection = function ($section) {
-            $state.go('section', {section: $section});
-        }
-    })
+            $scope.trustContent = function (html) {
+                return $sce.trustAsHtml(html);
+            };
+        })
 
-    .controller('SectionCtrl', function ($scope, $ionicHistory, $compile, $sce, $state, $stateParams, $http, $ionicModal, $timeout) {
-        if ($stateParams.section == null) {
-            $state.go('home');
-            $ionicHistory.clearHistory();
-            $ionicHistory.clearCache();
-        }
-        $scope.section = $stateParams.section;
+        .directive('bindHtmlCompile', ['$compile', function ($compile) {
+            return {
+                restrict: 'A',
+                link: function (scope, element, attrs) {
+                    scope.$watch(function () {
+                        return scope.$eval(attrs.bindHtmlCompile);
+                    }, function (value) {
+                        // Incase value is a TrustedValueHolderType, sometimes it
+                        // needs to be explicitly called into a string in order to
+                        // get the HTML string.
+                        element.html(value && value.toString());
+                        // If scope is provided use it, otherwise use parent scope
+                        var compileScope = scope;
+                        if (attrs.bindHtmlScope) {
+                            compileScope = scope.$eval(attrs.bindHtmlScope);
+                        }
+                        $compile(element.contents())(compileScope);
+                    });
+                }
+            };
+        }])
 
-        $http.get('http://' + $scope.section.url).then(function (res) {
-            $scope.sectionData = res.data;
-        }, function (err) {
-            console.error("HOME", err);
-        });
-
-        $scope.trustContent = function (html) {
-            return $sce.trustAsHtml(html);
-        };
-    })
-
-    .directive('bindHtmlCompile', ['$compile', function ($compile) {
-          return {
-              restrict: 'A',
-              link: function (scope, element, attrs) {
-                  scope.$watch(function () {
-                      return scope.$eval(attrs.bindHtmlCompile);
-                  }, function (value) {
-                      // Incase value is a TrustedValueHolderType, sometimes it
-                      // needs to be explicitly called into a string in order to
-                      // get the HTML string.
-                      element.html(value && value.toString());
-                      // If scope is provided use it, otherwise use parent scope
-                      var compileScope = scope;
-                      if (attrs.bindHtmlScope) {
-                          compileScope = scope.$eval(attrs.bindHtmlScope);
-                      }
-                      $compile(element.contents())(compileScope);
-                  });
-              }
-          };
-    }])
-
-;
+    ;
